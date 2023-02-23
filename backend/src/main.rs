9522867533142785020e23dbe23d65c120c9ed16
Anzle Chavez <anzlemchavez@gmail.com>
2023-02-23 11:04:37 -0500

    Basic struct and enum for client data

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,19 @@
+use chrono::prelude::*;
+// Create a struct that will represent the clients
+enum Membership {
+    MemberAuthority,
+    MemberRegular,
+    NonMember
+}
+struct Client {
+    first_name: String,
+    last_name: String,
+    member_join_date: DateTime<Utc>,
+    membership: Membership,
+}
+
+
 fn main() {
     println!("Hello, world!");
+    
 }
